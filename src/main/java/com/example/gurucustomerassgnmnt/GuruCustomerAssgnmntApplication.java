c4eade33409c7581259d4acbcdc8ec9cd2d8a884
XolaniDelman <Xolani.Delman@Absa.africa>
2022-09-07 23:26:13 +0200

    initial commit guru customer assessment

---
+++ src/main/java/com/example/gurucustomerassgnmnt/GuruCustomerAssgnmntApplication.java
@@ -0,0 +1,13 @@
+package com.example.gurucustomerassgnmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruCustomerAssgnmntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruCustomerAssgnmntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gurucustomerassgnmnt/GuruCustomerAssgnmntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gurucustomerassgnmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruCustomerAssgnmntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
